e97445d7a962ecc60fcfe4abb48b7a124982a679
phil413studio <95882066+phil413studio@users.noreply.github.com>
2025-04-16 23:08:01 +0100

    utilisateur model

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -1,14 +1,14 @@
 <?php
-
 namespace App\Entity;
 
 use App\Repository\UtilisateurRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
 #[ORM\Table(name: 'utilisateurs')]
+#[UniqueEntity(fields: ['email'], message: 'Cette adresse email est déjà utilisée.')]
 class Utilisateur
 {
     #[ORM\Id]
@@ -17,167 +17,31 @@ class Utilisateur
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Assert\NotBlank(message: 'L\'email ne peut pas être vide.')]
+    #[Assert\Email(message: 'L\'adresse email {{ value }} n\'est pas valide.')]
     private ?string $email = null;
 
-    #[ORM\Column(name: "role")]
-    private ?string $role = 'client';
+    /** @var string The hashed password */
+    #[ORM\Column]
+    private ?string $password = null; // Store hashed password
 
-    #[ORM\Column(name: "mot_de_passe_hash")]
-    private ?string $password = null;
-
-    #[ORM\Column(name: "mot_de_passe_sel")]
-    private ?string $salt = '';
-
-    #[ORM\Column(length: 255, name: "nom")]
+    #[ORM\Column(length: 100)]
+    #[Assert\NotBlank(message: 'Le nom ne peut pas être vide.')]
     private ?string $nom = null;
 
-    #[ORM\Column(length: 255, name: "prenom")]
+    #[ORM\Column(length: 100)]
+    #[Assert\NotBlank(message: 'Le prénom ne peut pas être vide.')]
     private ?string $prenom = null;
 
-    #[ORM\Column(length: 50, name: "telephone")]
-    private ?string $telephone = null;
-
-    #[ORM\OneToMany(mappedBy: 'utilisateur', targetEntity: Participation::class)]
-    private Collection $participations;
-
-    public function __construct()
-    {
-        $this->participations = new ArrayCollection();
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): static
-    {
-        $this->email = $email;
-        return $this;
-    }
-
-    public function getRoles(): array
-    {
-        // Map the role string to an array of roles
-        $roleMap = [
-            'admin' => ['ROLE_ADMIN', 'ROLE_USER'],
-            'client' => ['ROLE_USER'],
-            'partenaire' => ['ROLE_PARTNER', 'ROLE_USER']
-        ];
-
-        $roleKey = $this->getRole() ?? 'client';
-        return $roleMap[$roleKey] ?? ['ROLE_USER']; // Default to ROLE_USER
-    }
-
-    public function setRoles(array $roles): static
-    {
-        // Convert array to string role
-        if (in_array('ROLE_ADMIN', $roles)) {
-            $this->role = 'admin';
-        } elseif (in_array('ROLE_PARTNER', $roles)) {
-            $this->role = 'partenaire';
-        } else {
-            $this->role = 'client';
-        }
-        
-        return $this;
-    }
-
-    public function getRole(): ?string
-    {
-        return $this->role;
-    }
-
-    public function setRole(string $role): static
-    {
-        $this->role = $role;
-        return $this;
-    }
-
-    public function getPassword(): string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): static
-    {
-        // In a real application, you'd hash the password here
-        $this->password = $password;
-        // You might also set the salt
-        $this->salt = bin2hex(random_bytes(16));
-        return $this;
-    }
-
-    public function getSalt(): ?string
-    {
-        return $this->salt;
-    }
-
-    public function setSalt(string $salt): static
-    {
-        $this->salt = $salt;
-        return $this;
-    }
-
-    public function getNom(): ?string
-    {
-        return $this->nom;
-    }
-
-    public function setNom(string $nom): static
-    {
-        $this->nom = $nom;
-        return $this;
-    }
-
-    public function getPrenom(): ?string
-    {
-        return $this->prenom;
-    }
-
-    public function setPrenom(string $prenom): static
-    {
-        $this->prenom = $prenom;
-        return $this;
-    }
-
-    public function getTelephone(): ?string
-    {
-        return $this->telephone;
-    }
-
-    public function setTelephone(?string $telephone): static
-    {
-        $this->telephone = $telephone;
-        return $this;
-    }
-
-    public function getParticipations(): Collection
-    {
-        return $this->participations;
-    }
-
-    public function addParticipation(Participation $participation): static
-    {
-        if (!$this->participations->contains($participation)) {
-            $this->participations->add($participation);
-            $participation->setUtilisateur($this);
-        }
-        return $this;
-    }
-
-    public function removeParticipation(Participation $participation): static
-    {
-        if ($this->participations->removeElement($participation)) {
-            if ($participation->getUtilisateur() === $this) {
-                $participation->setUtilisateur(null);
-            }
-        }
-        return $this;
-    }
+    // Basic Getters & Setters
+    public function getId(): ?int { return $this->id; }
+    public function getEmail(): ?string { return $this->email; }
+    public function setEmail(string $email): static { $this->email = $email; return $this; }
+    public function getPassword(): ?string { return $this->password; }
+    public function setPassword(string $password): static { $this->password = $password; return $this; }
+    public function getNom(): ?string { return $this->nom; }
+    public function setNom(string $nom): static { $this->nom = $nom; return $this; }
+    public function getPrenom(): ?string { return $this->prenom; }
+    public function setPrenom(string $prenom): static { $this->prenom = $prenom; return $this; }
+    public function getFullName(): string { return $this->prenom . ' ' . $this->nom; }
 }
