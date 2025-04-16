ed35ba733dc8bd8f8f64a958f7027831bb0211d3
TakwaOueslati <149618914+TakwaOueslati@users.noreply.github.com>
2025-04-16 22:21:09 +0100

    Update PointRelais.php

--- src/Entity/PointRelais.php
+++ src/Entity/PointRelais.php
@@ -6,6 +6,36 @@ use App\Repository\PointRelaisRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
+#[ORM\Entity(repositoryClass: PointRelaisRepository::class)]
+class PointRelais
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'Le nom est obligatoire.')]
+    #[Assert\Length(
+        max: 255,
+        maxMessage: 'Le nom ne peut pas dépasser {{ limit }} caractères.'
+    )]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'L\'adresse est obligatoire.')]
+    #[Assert\Length(
+        max: 255,
+        maxMessage: 'L\'adresse ne peut pas dépasser {{ limit }} caractères.'
+    )]
+    private ?string $addresse = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    #[Assert\NotBlank(message: 'La date d\'ouverture est obligatoire.')]
+    #[Assert\Type(
+        type: \DateTimeInterface::class,
+        message: 'La valeur {{ value }} n\'est pas une date valide.'
+    )]
     private ?\DateTimeInterface $HoraireOuverture = null;
 
     public function getId(): ?int
