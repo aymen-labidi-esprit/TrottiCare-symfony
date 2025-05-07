b089e084bb8cf97f5e84edf1b1a841f53f7d962f
phil413studio <95882066+phil413studio@users.noreply.github.com>
2025-05-07 22:37:39 +0100

    add more  feature and merge

--- src/Entity/Reclamation.php
+++ src/Entity/Reclamation.php
@@ -1,65 +1,59 @@
 <?php
+// src/Entity/Reclamation.php
 
 namespace App\Entity;
 
 use App\Repository\ReclamationRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ReclamationRepository::class)]
-#[ORM\Table(name: 'reclamation')]
 class Reclamation
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    #[Assert\NotBlank(message: 'Le titre ne peut pas être vide.')]
-    #[Assert\Length(
-        min: 5,
-        max: 255,
-        minMessage: 'Le titre doit comporter au moins {{ limit }} caractères.',
-        maxMessage: 'Le titre ne peut pas dépasser {{ limit }} caractères.'
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: 'Le titre ne peut pas être vide')]
+    #[Assert\Length(max: 255, maxMessage: 'Le titre ne peut pas dépasser 255 caractères')]
+    #[Assert\Regex(
+        pattern: '/^[a-zA-Z\s&]+$/u',
+        message: 'Le titre ne doit contenir que des lettres, espaces et le caractère "&"'
     )]
-    private ?string $titre = null;
+    private string $titre;
 
     #[ORM\Column(type: 'text')]
-    #[Assert\NotBlank(message: 'La description ne peut pas être vide.')]
-    #[Assert\Length(
-        min: 10,
-        minMessage: 'La description doit comporter au moins {{ limit }} caractères.'
-    )]
-    private ?string $description = null;
+    #[Assert\NotBlank(message: 'La description ne peut pas être vide')]
+    private string $description;
 
-    #[ORM\Column(name: 'date_creation', type: 'datetime')]
-    #[Assert\NotNull(message: 'La date de création est requise.')]
-    #[Assert\LessThanOrEqual('now', message: 'La date de création ne peut pas être dans le futur.')]
-    private ?\DateTimeInterface $dateCreation;
+    #[ORM\Column(type: 'datetime')]
+    private \DateTimeInterface $date_creation;
 
-    #[ORM\Column(name: 'type_reclamation', length: 255)]
-    #[Assert\NotBlank(message: 'Le type de réclamation ne peut pas être vide.')]
-    #[Assert\Choice(
-        choices: ['TECHNIQUE', 'SERVICE', 'FACTURATION', 'AUTRE'],
-        message: 'Le type choisi n\'est pas valide. Types autorisés: TECHNIQUE, SERVICE, FACTURATION, AUTRE.'
-    )]
-    private ?string $typeReclamation = null;
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: 'Le type de réclamation est requis')]
+    private string $type_reclamation;
 
-    #[ORM\Column(name: 'user_email', length: 255)]
-    #[Assert\NotBlank(message: 'L\'email de l\'utilisateur ne peut pas être vide.')]
-    #[Assert\Email(
-        message: 'L\'adresse email {{ value }} n\'est pas valide.'
-    )]
-    #[Assert\Length(
-        max: 255,
-        maxMessage: 'L\'email ne peut pas dépasser {{ limit }} caractères.'
-    )]
-    private ?string $userEmail = null;
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: 'L\'email de l\'utilisateur est requis')]
+    #[Assert\Email(message: 'L\'email "{{ value }}" n\'est pas un email valide')]
+    private string $user_email;
+
+    #[ORM\Column(type: 'integer')]
+    #[Assert\NotBlank(message: 'L\'ID de la trottinette est requis')]
+    #[Assert\Positive(message: 'L\'ID de la trottinette doit être un nombre strictement positif')]
+    private int $id_trottinette;
+
+    #[ORM\OneToMany(mappedBy: 'reclamation', targetEntity: Reponse::class, cascade: ['persist', 'remove'])]
+    private Collection $reponses;
 
     public function __construct()
     {
-        $this->dateCreation = new \DateTime();
+        $this->reponses = new ArrayCollection();
+        $this->date_creation = new \DateTime();  // Date actuelle par défaut
     }
 
     public function getId(): ?int
@@ -91,34 +85,69 @@ class Reclamation
 
     public function getDateCreation(): ?\DateTimeInterface
     {
-        return $this->dateCreation;
+        return $this->date_creation;
     }
 
-    public function setDateCreation(\DateTimeInterface $dateCreation): self
+    public function setDateCreation(\DateTimeInterface $date_creation): self
     {
-        $this->dateCreation = $dateCreation;
+        $this->date_creation = $date_creation;
         return $this;
     }
 
     public function getTypeReclamation(): ?string
     {
-        return $this->typeReclamation;
+        return $this->type_reclamation;
     }
 
-    public function setTypeReclamation(string $typeReclamation): self
+    public function setTypeReclamation(string $type_reclamation): self
     {
-        $this->typeReclamation = $typeReclamation;
+        $this->type_reclamation = $type_reclamation;
         return $this;
     }
 
     public function getUserEmail(): ?string
     {
-        return $this->userEmail;
+        return $this->user_email;
     }
 
-    public function setUserEmail(string $userEmail): self
+    public function setUserEmail(string $user_email): self
     {
-        $this->userEmail = $userEmail;
+        $this->user_email = $user_email;
+        return $this;
+    }
+
+    public function getIdTrottinette(): ?int
+    {
+        return $this->id_trottinette;
+    }
+
+    public function setIdTrottinette(int $id_trottinette): self
+    {
+        $this->id_trottinette = $id_trottinette;
+        return $this;
+    }
+
+    public function getReponses(): Collection
+    {
+        return $this->reponses;
+    }
+
+    public function addReponse(Reponse $reponse): self
+    {
+        if (!$this->reponses->contains($reponse)) {
+            $this->reponses[] = $reponse;
+            $reponse->setReclamation($this);
+        }
+        return $this;
+    }
+
+    public function removeReponse(Reponse $reponse): self
+    {
+        if ($this->reponses->removeElement($reponse)) {
+            if ($reponse->getReclamation() === $this) {
+                $reponse->setReclamation(null);
+            }
+        }
         return $this;
     }
 }
--- src/Entity/Reponse.php
+++ src/Entity/Reponse.php
@@ -1,54 +1,86 @@
 <?php
 // src/Entity/Reponse.php
+
 namespace App\Entity;
 
-use App\Repository\ReponseRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
-use DateTimeInterface;
 
 #[ORM\Entity(repositoryClass: ReponseRepository::class)]
-#[ORM\Table(name: 'reponse_reclamation')] // Nom spécifique de la table
 class Reponse
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
-    private ?int $id = null;
+    private $id;
 
     #[ORM\ManyToOne(targetEntity: Reclamation::class, inversedBy: 'reponses')]
     #[ORM\JoinColumn(nullable: false)]
-    private ?Reclamation $reclamation = null;
-
-    #[ORM\ManyToOne(targetEntity: Utilisateur::class)]
-    #[ORM\JoinColumn(name: 'auteur_id', referencedColumnName: 'id', nullable: false)]
-    #[Assert\NotNull(message: "L'auteur de la réponse est requis.")]
-    private ?Utilisateur $auteur = null;
+    private $reclamation;
 
     #[ORM\Column(type: 'text')]
-    #[Assert\NotBlank(message: 'Le message ne peut pas être vide.')]
-    #[Assert\Length(min: 5, minMessage: 'La réponse doit faire au moins {{ limit }} caractères.')]
-    private ?string $message = null;
+    #[Assert\NotBlank(message: 'Le message ne peut pas être vide')]
+    private $message;
 
     #[ORM\Column(type: 'datetime')]
-    private ?DateTimeInterface $dateReponse = null;
+    private $date_reponse;
 
-    public function __construct()
-    {
-        $this->dateReponse = new \DateTime(); // Date de création par défaut
-    }
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: 'Le type d\'utilisateur est requis')]
+    private $user_type;
 
     // Getters and Setters
-    public function getId(): ?int { return $this->id; }
-    public function getReclamation(): ?Reclamation { return $this->reclamation; }
-    public function setReclamation(?Reclamation $reclamation): self { $this->reclamation = $reclamation; return $this; }
 
-    public function getAuteur(): ?Utilisateur { return $this->auteur; }
-    public function setAuteur(?Utilisateur $auteur): self { $this->auteur = $auteur; return $this; }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-    public function getMessage(): ?string { return $this->message; }
-    public function setMessage(string $message): self { $this->message = $message; return $this; }
+    public function getReclamation(): ?Reclamation
+    {
+        return $this->reclamation;
+    }
 
-    public function getDateReponse(): ?DateTimeInterface { return $this->dateReponse; }
-    public function setDateReponse(DateTimeInterface $dateReponse): self { $this->dateReponse = $dateReponse; return $this; }
+    public function setReclamation(?Reclamation $reclamation): self
+    {
+        $this->reclamation = $reclamation;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getDateReponse(): ?\DateTimeInterface
+    {
+        return $this->date_reponse;
+    }
+
+    public function setDateReponse(\DateTimeInterface $date_reponse): self
+    {
+        $this->date_reponse = $date_reponse;
+
+        return $this;
+    }
+
+    public function getUserType(): ?string
+    {
+        return $this->user_type;
+    }
+
+    public function setUserType(string $user_type): self
+    {
+        $this->user_type = $user_type;
+
+        return $this;
+    }
 }
